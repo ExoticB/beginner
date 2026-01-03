90f9a226f56e1fb94e4bfb0c3fab34b256e8552d
ExoticB <stevenbezan7@gmail.com>
2026-01-03 04:48:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,75 @@
-import 'package:flutter/material.dart';
+**Create variables of different types and perform operations
+int age = 15;
+double height = 5.6;
+String name = "Alex";
+bool isStudent = true;
+int nextYearAge = age + 1;
+double heightInCm = height * 30.48;
+************************************
+**Calculate the area of a circle using constants
+  const double pi = 3.14159;
+double radius = 5;
+double area = pi * radius * radius
+************************************
+**Use string interpolation to create a greeting message
+  String greeting = "Hello, my name is $name and I am $age years old."
+************************************
+**Calculator
+  void main() {
+  double num1 = 10.5;
+  double num2 = 5.0;
+  String operator = "/"; 
 
-void main() {
-  runApp(const MyApp());
+  double result;
+  if (operator == "+") {
+    result = num1 + num2;
+    print("النتيجة: $num1 + $num2 = $result");
+  } 
+  else if (operator == "-") {
+    result = num1 - num2;
+    print("النتيجة: $num1 - $num2 = $result");
+  } 
+  else if (operator == "*") {
+    result = num1 * num2;
+    print("النتيجة: $num1 × $num2 = $result");
+  } 
+  else if (operator == "/") {
+    if (num2 != 0) {
+      result = num1 / num2;
+      print("النتيجة: $num1 ÷ $num2 = $result");
+    } else {
+      print("erorr");
+    }
+  } 
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    int age = 15;
-    double radius = 5;
-
-    const double pi = 3.14159;
-    double area = pi * radius * radius;
-
-    String message =
-        "Hello! I am $age years old.\n"
-        "Area of the circle is $area";
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter Beginner App"),
-        ),
-        body: Center(
-          child: Text(
-            message,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 18),
-          ),
-        ),
-      ),
-    );
+************************************
+void main() {
+  int dayNumber = 3;
+  switch (dayNumber) {
+    case 1:
+      print("Monday (الأحد)");
+      break;
+    case 2:
+      print("Tuesday (الإثنين)");
+      break;
+    case 3:
+      print("Wednesday (الثلاثاء)");
+      break;
+    case 4:
+      print("Thursday (الأربعاء)");
+      break;
+    case 5:
+      print("Friday (الخميس)");
+      break;
+    case 6:
+      print("Saturday (الجمعة)");
+      break;
+    case 7:
+      print("Sunday (السبت)");
+      break;
+    default:
+      print("Invalid input (مدخل غير صالح)");
   }
 }
+************************************
+  
